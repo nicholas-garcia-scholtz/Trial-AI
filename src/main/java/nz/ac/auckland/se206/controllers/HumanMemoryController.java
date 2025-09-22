3acd31d151341ef59bb7a74efab56157e63a09a8
kylerosser <39887097+kylerosser@users.noreply.github.com>
2025-09-22 14:13:12 +1200

    Create images and fxml and controller for Human memory

---
+++ src/main/java/nz/ac/auckland/se206/controllers/HumanMemoryController.java
@@ -0,0 +1,13 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class HumanMemoryController {
+  @FXML private ImageView artworkLayer1;
+  @FXML private ImageView artworkLayer2;
+  @FXML private ImageView artworkLayer3;
+  @FXML private ImageView bubble1;
+  @FXML private ImageView bubble2;
+  @FXML private ImageView bubble3;
+}
